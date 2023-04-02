c84ae972847dcc0f44bd877545ee9e1950f16b83
salsabillalula <salsabillalula@gmail.com>
2023-04-02 14:36:12 +0700

    chatbot-pa-1

--- chatbot-backend/main.py
+++ chatbot-backend/main.py
@@ -1,38 +1,72 @@
 from telebot import *
 import re
 import random
+import telebot
 
 api = '5994491197:AAFEhWxSNOSGs8jOTEcZf4-hqnvRvKsjPdE'
 bot = TeleBot(api)
 
+
+# Dataset gejala dan diagnosis
+dataset = {
+    'cemas ' : 'OCD',
+    'capek': 'gangguan kecemasan',
+    'gejala3': 'diagnosis3',
+    # dan seterusnya
+}
+
+# Aturan-aturan berdasarkan gejala
+aturan = {
+    'cemas': ['OCD'],
+    'gejala2': ['diagnosis2'],
+    'gejala3': ['diagnosis3'],
+    'cemas+ capek': ['gangguan kecemasan'],
+    'gejala1+gejala3': ['diagnosis5'],
+    # dan seterusnya
+}
+
 # start
 @bot.message_handler(commands=['start'])
 def selamat_datang(message):
-    bot.reply_to(message, 'Selamat Datang di Chatbot Layanan Konseling Politeknik Elektronika Negeri Surabaya')
-    chatid = message.chat.id
-    bot.send_message(chatid, 'Pilih Opsi Terkait Layanan Chatbot Konseling : \n'
-                     '1. Sistem Diagnosis Kesehatan Mental \n'
-                     '2. Sistem Informasi Kesehatan Mental')
+    name = message.from_user.full_name
+    bot.reply_to(message, f"Selamat Datang {name} di Chatbot Layanan Konseling Politeknik Elektronika Negeri Surabaya")
+    markup = types.ReplyKeyboardMarkup()
+    item1 = types.KeyboardButton('/start')
+    item2 = types.KeyboardButton('/informasi')
+    item3 = types.KeyboardButton('/diagnosis')
+    item4 = types.KeyboardButton('/profile')
+    markup.row(item1,item2)
+    markup.row(item3, item4)
+    
+    # chatid = message.chat.id
+    bot.reply_to(message, 'Pilih Opsi Terkait Layanan Chatbot Konseling', reply_markup=markup)
+    #                  '1. Sistem Diagnosis Kesehatan Mental \n'
+    #                  '2. Sistem Informasi Kesehatan Mental')
     #bot.send_message(chatid, 'Terimakasih telah mengakses chatbot layanan konsultasi PENS. Apakah anda memiliki permasalahan, keluhan atau hal hal yang menjadi concern dalam masalah kesehatan mental anda? Silahkan sharing terkait apa yang sedang anda alami dan gejala yang anda rasakan.')
 
-#chatbot
-#answer = ['Terimakasih telah mengakses chatbot layanan konsultasi PENS. Apakah anda memiliki permasalahan, keluhan atau hal hal yang menjadi concern dalam masalah kesehatan mental anda? Silahkan sharing terkait apa yang sedang anda alami dan gejala yang anda rasakan.']
-@bot.message_handler(content_types=['text'])
-def chatbot(message):
-     teks = message.text 
-     #if re.findall('halo|hai', teks) :
-     if re.findall('1|satu|Satu|Diagnosis|diagnosis', teks) :
-         chatid = message.chat.id
-         bot.send_message(chatid, 'Terimakasih telah mengakses chatbot layanan konsultasi PENS. Apakah anda memiliki permasalahan, keluhan atau hal hal yang menjadi concern dalam masalah kesehatan mental anda? Silahkan sharing terkait apa yang sedang anda alami dan gejala yang anda rasakan.') 
-     elif re.findall('2|dua|Dua|Informasi|informasi', teks) :
-         chatid = message.chat.id
-         bot.send_message(chatid, '1. Anxiety \n'
-                          '2. Depresi')
-         if re.findall('1|satu|Satu|Anxiety|anxiety', teks) :
-            chatid = message.chat.id
-            bot.send_message(chatid, 'Anxiety adalah')
-     else :
-         chatid = message.chat.id
-         bot.send_message(chatid, 'Nggak paham')
+# Fungsi untuk mencari diagnosis berdasarkan gejala
+@bot.message_handler(commands=['diagnosis'])
+def diagnosis(message) :
+    bot.reply_to(message, 'Terimakasih telah mengakses chatbot layanan konsultasi PENS. Apakah anda memiliki permasalahan, keluhan atau hal hal yang menjadi concern dalam masalah kesehatan mental anda? Silahkan sharing terkait apa yang sedang anda alami dan gejala yang anda rasakan.')
+def cari_diagnosis(gejala):
+    diagnosis = []
+    for k in aturan:
+        if all(g in gejala for g in k.split('+')):
+            diagnosis += aturan[k]
+    return diagnosis
 
-bot.polling(True)
+# Fungsi untuk menangani pesan dari pengguna
+@bot.message_handler(func=lambda message: True)
+def handle_message(message): 
+    # Mendapatkan gejala dari pesan pengguna
+    gejala = message.text.lower()
+    # Mencari diagnosis berdasarkan gejala
+    hasil_diagnosis = cari_diagnosis(gejala)
+    # Memberikan respons ke pengguna
+    if hasil_diagnosis:
+        bot.reply_to(message, 'Diagnosis yang mungkin terjadi: {}'.format(', '.join(hasil_diagnosis)))
+    else:
+        bot.reply_to(message, 'Maaf, tidak dapat menemukan diagnosis yang sesuai dengan gejala yang diberikan.')
+
+# Menjalankan bot
+bot.polling()
